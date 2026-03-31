695ce4f7da681168a417f48528146c87478bc974
Shreya <shreya501711@gmail.com>
2026-03-31 14:03:50 +0000

    fix navbar hamburger button

--- sections/navbar.js
+++ sections/navbar.js
@@ -1,55 +1,38 @@
 class MyNavbar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <nav class="navbar navbar-expand-lg navbar-dark">
-  <div class="container py-2 px-3">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-black">
+          <div class="container py-2 px-3">
+            <a class="navbar-brand" href="#hero">The Wagtail Bakery</a>
+            <button class="navbar-toggler" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="#breads">Breads</a></li>
+                <li class="nav-item"><a class="nav-link" href="#locations">Locations</a></li>
+                <li class="nav-item"><a class="nav-link" href="#blog">Blog</a></li>
+                <li class="nav-item"><a class="nav-link" href="#recipes">Recipes</a></li>
+                <li class="nav-item"><a class="nav-link" href="#gallery">Gallery</a></li>
+                <li class="nav-item"><a class="nav-link" href="#contact">Contact Us</a></li>
+                <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+              </ul>
+            </div>
+          </div>
+        </nav>`
 
-    <a class="navbar-brand" href="#hero">The Wagtail Bakery</a>
+        const btn = this.querySelector('.navbar-toggler')
+        const menu = this.querySelector('#navbarNav')
 
-    <button class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-  <li class="nav-item">
-    <a class="nav-link" href="#breads">Breads</a>
-  </li>
-
-  <li class="nav-item">
-    <a class="nav-link" href="#locations">Locations</a>
-  </li>
-
-  <li class="nav-item">
-    <a class="nav-link" href="#blog">Blog</a>
-  </li>
-
-  <li class="nav-item">
-    <a class="nav-link" href="#recipes">Recipes</a>
-  </li>
-
-  <li class="nav-item">
-    <a class="nav-link" href="#gallery">Gallery</a>
-  </li>
-
-  <li class="nav-item">
-    <a class="nav-link" href="#contact">Contact Us</a>
-  </li>
-
-  <li class="nav-item">
-    <a class="nav-link" href="#about">About</a>
-  </li>
-
-</ul>
-
-    </div>
-
-  </div>
-</nav>
-        `
+        if (btn && menu) {
+            btn.addEventListener('click', () => {
+                if (menu.classList.contains('show')) {
+                    menu.classList.remove('show')
+                } else {
+                    menu.classList.add('show')
+                }
+            })
+        }
     }
 }
 
