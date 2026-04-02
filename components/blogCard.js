a6253c3fe255a9038cd35e51a27c25352910fe70
Shreya <shreya501711@gmail.com>
2026-04-02 12:59:16 +0000

    make normal blogs stack vertically on smaller screens and add icons for author and date

--- components/blogCard.js
+++ components/blogCard.js
@@ -10,23 +10,23 @@ class BlogCard extends HTMLElement {
         const newest = this.getAttribute("newestBlog") === "true"
         const imageVariant = newest ? "large-image" : "medium-image"
         
-        const layoutDirection = newest ? "flex-column flex-lg-row" : "flex-row flex-lg-column"
+        const layoutDirection = newest ? "flex-column flex-lg-row" : "flex-column"
 
-        const imgCol = newest ? "col-lg-6 col-12" : "col-lg-12 col-5" 
-        const textCol = newest ? "col-lg-6 col-12" : "col-lg-12 col-7"
+        const imgCol = newest ? "col-lg-6 col-12" : "col-12" 
+        const textCol = newest ? "col-lg-6 col-12" : "col-12"
 
         const headingSize = newest ? "display-5" : "h6"
         
         this.innerHTML = `
-    <div class="d-flex ${layoutDirection} align-items-stretch mb-5 gap-3">
+    <div class="d-flex ${layoutDirection} align-items-stretch mb-5 gap-3 px-2">
         <div class="${imgCol}">
             <my-image src="${src}" alt="${alt}" variant="${imageVariant}"></my-image>
         </div>
-        <div class="${textCol} d-flex flex-column justify-content-center ps-2">
-            <h5 class="text-white text-start ${headingSize} mb-1 mt-lg-2">${blogTitle}</h5>
-            <p class="text-white-50 text-start m-0 small"><strong>By</strong> ${blogAuthor}</p>
-            <p class="text-white text-start m-0 lh-sm small">${blogDesc}</p>
-            <p class="text-white-50 text-start small mt-2 mb-0">${blogDate}</p>
+        <div class="${textCol} d-flex flex-column justify-content-center">
+            <h5 class="text-white text-start ${headingSize} mb-1 mt-2">${blogTitle}</h5>
+            <p class="text-white-50 text-start small mt-2 mb-0"><i class="bi bi-calendar-fill"></i>&emsp;${blogDate}</p>
+            <p class="text-white text-start m-0 small mt-1"><i class="bi bi-feather"></i>&emsp;${blogAuthor}</p>
+            <p class="text-white-85 text-start m-0 lh-sm small">${blogDesc}</p>
         </div>
     </div>
 `
